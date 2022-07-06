6b9e7a080963c681fa0364c8d145620a9bf0d438
Corvus <gdiaz1092@gmail.com>
2022-07-06 16:09:56 -0400

    gitignored complete

---
+++ MyFirstDotnet/MyFirstDotnet/Program.cs
@@ -0,0 +1,11 @@
+// This is a comment
+
+﻿// See https://aka.ms/new-console-template for more information
+
+// Concole.WriteLine is from the system library. Allows us to write text onto console. 
+// (___) Parameters. These say we are sending Console.WriteLine whatever is inside the ().
+//"" denote a string letteral.
+// ; Endline Character.
+
+Console.WriteLine("Hello, World!");
+
