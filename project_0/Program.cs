279deab00834e13ae226db9762f921c64de1d3b2
Corvus <gdiaz1092@gmail.com>
2022-07-10 18:24:17 -0400

    fixed MultiD Array

---
+++ project_0/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+// Let user choose between player 2 or computer player.
+// Add win conditions
+// Not able to overwrite previous moves.
+// after every match ask player if they would like to continue if so run new instance. Otherwise end.
+
+
+namespace Project0
+{
+    class TicTacToe
+     {
+        static void Main(string[] args)
+        {
+            // Declare gameBoard and print out to console.
+            char[,] gameBoard = new char[,] {{'.','.','.'},{'.','.','.' },{'.','.','.'}};
+            for (int row = 0; row < 3; row++)
+            {
+                 for (int col = 0; col < 3 ; col++) 
+                 {
+                    Console.Write(gameBoard[row , col] + " ");
+                 }
+
+                 Console.WriteLine();
+            }
+         
+
+            //int[] gameBoard = new int[9];
+
+
+            // For loop to run the length of the INT array gameBoard
+          //  for (int i = 0; i < 9 ; i++)
+           // {
+          //      gameBoard[i] = 0;
+           // }
+
+
+            // Declare counter variable
+           // int counter = 0;
+
+
+         //   while (counter <= 8)
+           // {
+
+          //  }
+            //for (int i = 0; i <= gameBoard.Length; i++);
+        }
+
+    }
+}
